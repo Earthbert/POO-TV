cbf97361c2af33ab1ca781ba0c8dba4f46f034e0
Earthbert <daraban_timotei@tutanota.com>
2022-12-05 13:11:12 +0200

    Made Database.instance field unique per thread.

--- src/database/Database.java
+++ src/database/Database.java
@@ -5,12 +5,11 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import movie.Movie;
 import user.User;
-
 import java.util.ArrayList;
 import java.util.List;
 
 final public class Database {
-    private static Database instance;
+    private static final ThreadLocal<Database> instance = new ThreadLocal<>();
     private final List<User> users;
     private final List<Movie> movies;
     private final List<Action> actions;
@@ -19,14 +18,18 @@ final public class Database {
     private Database(@JsonProperty("users") final ArrayList<User> users,
                     @JsonProperty("movies") final ArrayList<Movie> movies,
                     @JsonProperty("actions") final ArrayList<Action> actions) {
-        instance = this;
+        if (instance.get() != null) {
+            System.err.println("Multiple Instances of Database");
+            System.exit(-1);
+        }
+        instance.set(this);
         this.users = users;
         this.movies = movies;
         this.actions = actions;
     }
 
     public static Database getInstance() {
-        return instance;
+        return instance.get();
     }
 
     public List<User> getUsers() {
